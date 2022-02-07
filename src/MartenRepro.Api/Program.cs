a3a5ff0593491abdde553a79a4901d765e668580
Tomas Jansson <tomas.jansson@gmail.com>
2022-02-07 22:05:07 +0100

    More cleanup

--- src/MartenRepro.Api/Program.cs
+++ src/MartenRepro.Api/Program.cs
@@ -13,11 +13,7 @@ using Microsoft.Identity.Web;
 using Weasel.Postgresql;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllers();
 builder.Services.AddCarter();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddScoped<IEventStore, MartenStore>();
 builder.Services.AddScoped<Application, Application>(sp =>
 {
@@ -25,59 +21,16 @@ builder.Services.AddScoped<Application, Application>(sp =>
 });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
-builder.Services.AddAuthorization(opts =>
-{
-    opts.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-        .RequireAuthenticatedUser()
-        .Build();
-});
 
 builder.Services.AddMarten(options =>
 {
     var connectionString = "Host=localhost;Port=5432;Database=MartenRepro;Username=martenrepro;password=password";
     options.Connection(connectionString);
 
-    // Use the morp permissive schema auto create behavior
-    // while in development
-    // if (app.Hosting.IsDevelopment())
-    // {
     options.AutoCreateSchemaObjects = AutoCreate.CreateOrUpdate; // change to None when we stabilize db structure
-    // }
-
-    // Register projections
     options.Projections.Add<TemplateFolderBuilder>(Marten.Events.Projections.ProjectionLifecycle.Inline);
 });
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(b =>
-    {
-        b.AllowAnyOrigin();
-        b.AllowAnyHeader();
-        b.AllowAnyMethod();
-    });
-});
-
 var app = builder.Build();
-
-app.UseForwardedHeaders();
-
-// app.UseHttpsRedirection();
-
-app.UseCors();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else {
-    app.UseAuthorization();
-}
-
-app.MapControllers();
 app.MapCarter();
 
 app.Run();
